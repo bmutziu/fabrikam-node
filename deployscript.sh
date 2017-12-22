115a074f3e6ecbf9eee5b5e86ddf439b5183dc71
Bogdan Mutiu <bmutiu001@cee.ema.ad.pwcinternal.com>
2017-12-22 21:47:36 +0200

    Good version to deploy for CentOS

--- deployscript.sh
+++ deployscript.sh
@@ -1,1 +1,11 @@
-echo "Starting deployment script run" && echo "End of script run"
+echo "Starting deployment script run"
+sudo yum check-update
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum install -y nodejs
+sudo npm install
+sudo yum install -y nginx
+sudo rm -f /etc/nginx/conf.d/default.conf && sudo cp node-app-nginx-config /etc/nginx/conf.d/node-app-nginx-config.conf
+sudo service nginx restart
+sudo npm install -g pm2 && sudo pm2 start -f app.js
+# sudo pm2 startup systemd && sudo pm2 save
+echo "End of script run"
